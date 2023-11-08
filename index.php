68b080a10ac7dc7a58844d45010506bf73cb28fc
Mathias Barbier <mathbarbier07@gmail.com>
2023-11-08 12:45:34 +0100

    fix

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-$message = isset($_GET['message']) ? $_GET['message'] : '';
-$name = isset($_GET['name']) ? $_GET['name'] : '';
-$lastName = isset($_GET["lastName"]) ? $_GET["lastName"] : '';
-$gender = isset($_GET['gender']) ? $_GET(['gender']) : '';
-$email = isset($_GET['email']) ? $_GET['email'] : '';
-$country = isset($_GET['country']) ? $_GET['country'] : '';
+require "./assets/php/form.php";
 
 
 ?>
